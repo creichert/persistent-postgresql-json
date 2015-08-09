1297063c312ef4e89c9cd0c5f56d4f7a063b9fbe
Christopher Reichert <creichert07@gmail.com>
2015-08-09 14:05:06 -0500

    Postgresql json/jsonb helpers for persistent

---
+++ Database/Persist/Postgresql/Json.hs
@@ -0,0 +1,173 @@
+
+-- | A simple json/jsonb persistent type
+
+{-# LANGUAGE ExistentialQuantification  #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE TypeFamilies               #-}
+
+module Database.Persist.Postgresql.Json
+       (
+         Jsonb(..)
+       , Json(..)
+       , (@>.)
+       , (<@.)
+       ) where
+
+import           Data.Aeson
+import qualified Data.Aeson           as A
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.Text.Encoding   as TE
+import           Data.Time            ()
+import           Database.Persist
+import           Database.Persist.Sql
+
+
+-- | Postgresql JSON type
+newtype Json
+  = Json A.Value
+    deriving (
+               Show
+             , Eq
+             , FromJSON
+             , ToJSON )
+
+
+instance PersistField Json where
+  toPersistValue v = PersistText $ TE.decodeUtf8 $ BSL.toStrict $ A.encode v
+  fromPersistValue (PersistByteString v)
+    = case A.decode (BSL.fromStrict v) of
+        Nothing -> Left "Invalid JSON"
+        Just j  -> Right j
+  fromPersistValue _ = Left "Not PersistText"
+
+instance PersistFieldSql Json where
+   sqlType _ = SqlOther "JSON"
+
+-- | Postgresql JSONB type
+newtype Jsonb
+  = Jsonb A.Value
+    deriving (
+               Show
+             , Eq
+             , FromJSON
+             , ToJSON )
+
+instance PersistField Jsonb where
+  toPersistValue v = PersistText $ TE.decodeUtf8 $ BSL.toStrict $ A.encode v
+  fromPersistValue (PersistByteString v)
+    = case A.decode (BSL.fromStrict v) of
+        Nothing -> Left "Invalid jsonb"
+        Just j  -> Right j
+  fromPersistValue _ = Left "Invalid PersistValue for JSONB. PersistByteString required."
+
+instance PersistFieldSql Jsonb where
+   sqlType _ = SqlOther "JSONB"
+
+infixr 6 @>., <@.
+
+-- | Does the left JSON value contain within it the right value?
+--
+-- > '{"a":1, "b":2}'::jsonb @> '{"b":2}'::jsonb
+--
+-- See tests for example usage
+(@>.) :: EntityField record Jsonb -> A.Value -> Filter record
+(@>.) field val = Filter field jval specifier
+  where
+    specifier = BackendSpecificFilter "@>"
+    jval = Left $ Jsonb val
+
+-- | Does the left JSON value contain within it the right value?
+--
+-- > '{"a":1, "b":2}'::jsonb <@ '{"b":2}'::jsonb
+--
+-- See tests for example usage
+(<@.) :: EntityField record Jsonb -> A.Value -> Filter record
+(<@.) field val = Filter field jval specifier
+  where
+    specifier = BackendSpecificFilter "<@"
+    jval = Left $ Jsonb val
+
+
+{-
+
+FIX Some operators cannot yet be defined due to having a ? which
+is used in persistent's query interpolation syntax.
+
+(?.) :: EntityField record Jsonb -> A.Value -> Filter record
+(?.) field val = Filter field jval specifier
+  where
+    specifier = BackendSpecificFilter " ? "
+    jval = Left $ Jsonb val
+
+
+(?|.) :: EntityField record Jsonb -> [A.Value] -> Filter record
+(?|.) field val = Filter field jval specifier
+  where
+    specifier = BackendSpecificFilter "\?| "
+    jval = Right $ fmap Jsonb val
+
+
+(?&.) :: EntityField record Jsonb -> [A.Value] -> Filter record
+(?&.) field val = Filter field jval specifier
+  where
+    specifier = BackendSpecificFilter " \"?&\" "
+    jval = Right $ fmap Jsonb val
+
+
+-- * BackendSpecificUpdate not supported in persistent-postgresql.
+
+-- postgresql 9.5 only
+f -. a = Update f a (BackendSpecificUpdate "-")
+
+-}
+
+
+{- Some helpful orphan instances
+
+instance PersistField A.Value where
+    toPersistValue (A.Object obj) =
+      PersistMap . fmap (\(k,v) -> (k, toPersistValue v)) . M.toList $ obj
+    toPersistValue (A.Array arr)  = PersistList . fmap toPersistValue . V.toList $ arr
+    toPersistValue (A.String text)= PersistText text
+    toPersistValue (A.Number num) = toPersistValue num
+    toPersistValue (A.Bool bool)  = PersistBool bool
+    toPersistValue (A.Null)       = PersistNull
+
+    fromPersistValue (PersistText text)     = Right $ A.String text
+    fromPersistValue (PersistByteString bs) = Right . A.String . E.decodeUtf8 $ bs
+    fromPersistValue (PersistInt64 int) = Right $ A.Number $ fromIntegral int
+    fromPersistValue (PersistDouble doub) = Right . A.Number $ fromRational $ toRational doub
+    fromPersistValue (PersistBool bool)     = Right $ A.Bool bool
+    fromPersistValue (PersistDay day)       = Right . A.String . pack . show $ day
+    fromPersistValue (PersistTimeOfDay time)= Right . A.String . pack . show $ time
+    fromPersistValue (PersistUTCTime utc)   = Right . A.String . pack . show $ utc
+    fromPersistValue (PersistNull)          = Right A.Null
+    fromPersistValue (PersistMap listPairs) =
+      let parsePair (k,v) = case fromPersistValue v of
+                              Right s -> Right (k,s)
+                              Left m  -> Left m
+      in (A.Object . M.fromList) `liftM` mapM parsePair listPairs
+    -- fromPersistValue (PersistInt64 int) = Right . A.Number . fromIntegral $ int
+    -- fromPersistValue (PersistInt64 int)     = Right . A.Number . N.I . fromIntegral $ int
+    -- fromPersistValue (PersistObjectId bs)   = Right . String . E.decodeUtf8 $ bs
+    --- fromPersistValue (PersistList vals)     = (Array . V.fromList) `liftM` (mapM (fromPersistValue) vals)
+
+
+
+instance PersistField Scientific where
+    toPersistValue = PersistRational . toRational
+    fromPersistValue (PersistRational r) = Right $ fromRational r
+    fromPersistValue (PersistDouble d) = Right $ fromFloatDigits d
+    fromPersistValue (PersistInt64 i) = Right $ fromIntegral i
+    fromPersistValue x = Left $ T.pack
+        "PersistField Scientific: Expected Scientific, received: " <> T.pack (show x)
+
+instance PersistFieldSql Scientific where
+  sqlType _ = SqlNumeric 32 20
+
+-}
---
+++ test/Spec.hs
@@ -0,0 +1,137 @@
+
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE NoMonomorphismRestriction  #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Control.Monad.Logger
+import           Control.Monad.Trans.Resource
+import qualified Data.Aeson                       as A
+import           Data.Text                        (Text)
+import           Database.Persist
+import           Database.Persist.Postgresql
+import           Database.Persist.TH
+import           System.Log.FastLogger
+
+import           Test.Hspec
+import           Test.HUnit                       (Assertion, assertBool,
+                                                   assertFailure, (@=?), (@?=))
+
+import           Database.Persist.Postgresql.Json
+
+
+share [ mkPersist sqlSettings
+      , mkMigrate "migration"
+      ] [persistLowerCase|
+Foo
+  json  Jsonb
+  deriving Show Eq
+
+Bar
+  json  Json
+  deriving Show Eq
+|]
+
+
+db :: SqlPersistT (LoggingT (ResourceT IO)) () -> Assertion
+db actions = runResourceT $ runConn (actions >> cleanDB)
+  where
+    cleanDB = deleteWhere ([] :: [Filter Foo])
+              >> deleteWhere ([] :: [Filter Bar])
+
+
+assertNotEqual :: (Eq a, Show a) => String -> a -> a -> Assertion
+assertNotEqual preface expected actual =
+    unless (actual /= expected) (assertFailure msg)
+  where
+    msg = (if null preface then "" else preface ++ "\n") ++
+          "expected: " ++ show expected ++ "\n to not equal: " ++ show actual
+
+
+runConn f = flip runLoggingT logit $
+    withPostgresqlPool connstr 1 $ runSqlPool f
+  where
+    connstr = "host=localhost port=5432 password=test user=test dbname=test"
+    logit _ _ _ s = print $ fromLogStr s
+
+io = liftIO
+
+infix 1 @/= --, /=@
+infix 1 @==, ==@
+
+(@/=), (@==), (==@) :: (Eq a, Show a, MonadIO m) => a -> a -> m ()
+actual   @/= expected = io $ assertNotEqual "" expected actual
+expected @== actual   = io $ expected @?= actual
+expected ==@ actual   = io $ expected @=? actual
+
+
+main :: IO ()
+main = do
+  runResourceT $ runConn $ runMigration migration
+  hspec specs
+
+pjsonb = Jsonb $ A.object [ "key" A..= ("value"::Text) ]
+pjson  = Json  $ A.object [ "key" A..= ("value"::Text)
+                          , "key2" A..= A.object [ "nested" A..= (""::Text) ]
+                          ]
+
+
+specs :: Spec
+specs = do
+  describe "persistent-postgresql json" $ do
+    it "inserts json" $ db $ do
+      let bar= Bar pjson
+      k <- insert bar
+      bars <- selectList ([] :: [Filter Bar]) []
+      bars @== [Entity k bar]
+
+  describe "persistent-postgresql jsonb" $ do
+    it "inserts jsonb" $ db $ do
+      let foo = Foo pjsonb
+      k <- insert foo
+      foos <- selectList ([] :: [Filter Foo]) []
+      foos @== [Entity k foo]
+
+    it "postgresql jsonb operator @>" $ db $ do
+      let foo = Foo pjsonb
+      k <- insert foo
+      foos <- selectList [FooJson @>. A.object [ "key" A..= ("value"::Text)] ] []
+      foos @== [Entity k foo]
+      foos' <- selectList [FooJson @>. A.object [ "ky" A..= ("alue"::Text)] ] []
+      foos' @== []
+
+    it "postgresql jsonb operator <@" $ db $ do
+      let foo = Foo pjsonb
+      k <- insert foo
+      foos <- selectList [FooJson <@. A.object [ "key" A..= ("value"::Text)] ] []
+      foos @== [Entity k foo]
+
+    -- not working to do query placeholder syntax
+
+    -- it "?" $ db $ do
+    --   let foo = Foo pjsonb
+    --   k <- insert foo
+    --   foos <- selectList [FooJson ?. "key"] []
+
+    -- it "?|" $ db $ do
+    --   let foo = Foo pjsonb
+    --   k <- insert foo
+    --   foos <- selectList [FooJson ?|. [ "key" ]] []
+    --   foos @== [Entity k foo]
+
+    -- it "?&" $ db $ do
+    --   let foo = Foo pjsonb
+    --   k <- insert foo
+    --   foos <- selectList [FooJson ?&. [ "key", "key2" ]] []
+    --   foos @== [Entity k foo]
